6cb003ae6d6f3621de65084862c853f8090ad34d
Dan Andrei Tutunea <dan.andrei.tutunea@icloud.com>
2017-03-10 09:23:37 +0000

    removed unused vsriable

--- src/Path.java
+++ src/Path.java
@@ -3,14 +3,13 @@ public class Path {
 
     
     private int lhsWheelSpeed, rhsWheelSpeed;
-    private int timeMiliSeconds, timeSeconds;
+    private int timeMiliSeconds;
 
     Path(int lhs, int rhs, int time){
         
         lhsWheelSpeed = lhs;
         rhsWheelSpeed = rhs;
         timeMiliSeconds = time;
-        timeSeconds = time / 1000;
     }
 
     public int getLHSspeed(){
